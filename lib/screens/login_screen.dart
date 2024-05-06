ed873de74fe9cb334e3cfe2e16ba704ba8af5d0f
Lucas Borges <luborn.dev@gmail.com>
2024-05-06 15:43:02 -0300

    feat: login screen implemented

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _rememberPassword = false;
+  final _formKey = GlobalKey<FormState>();
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      // Process login logic
+      Navigator.pushReplacementNamed(context, '/');
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/main');
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  _buildTextField(_usernameController, 'Usuário'),
+                  const SizedBox(height: 16), // Spacing between fields
+                  _buildTextField(_passwordController, 'Senha', obscureText: true),
+                  const SizedBox(height: 8), // Spacing between the field and the "forgot password" text
+                  Align(
+                    alignment: Alignment.center,
+                    child: TextButton(
+                      onPressed: () {
+                        // Handle forgot password logic
+                      },
+                      child: const Text(
+                        'Esqueci a senha',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: _rememberPassword,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _rememberPassword = value ?? false;
+                          });
+                        },
+                      ),
+                      const Text('Remember password'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24), // Gap before the login button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  side: BorderSide(width: 2, color: Colors.black),
+                ),
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                minimumSize: const Size(160, 40),
+              ),
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label, {bool obscureText = false}) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxWidth: 280,
+      ),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(width: 2, color: Colors.black),
+          ),
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Por favor, insira $label';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
